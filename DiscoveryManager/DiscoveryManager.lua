42accea1b8ace09e27efe4a90140c4d96b40c830
lshtech <elbebeta@gmail.com>
2024-10-11 20:43:42 -0500

    Update DiscoveryManager.lua

--- DiscoveryManager/DiscoveryManager.lua
+++ DiscoveryManager/DiscoveryManager.lua
@@ -58,120 +58,11 @@ function config_tab()
             colour = G.C.BLACK
         },
         nodes = {
-            UIBox_button({label = {"Unlock/Discover All"}, button = "discover_all", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
-            UIBox_button({label = {"Reset"}, button = "reset_cards", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
-            UIBox_button({label = {"Clear Alerts"}, button = "clear_alerts", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
+            UIBox_button({label = {"Unlock/Discover All"}, button = "unlock_all", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
+            UIBox_button({label = {"Reset"}, button = "undiscover_all", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
+            UIBox_button({label = {"Clear Alerts"}, button = "clear_all", colour = G.C.ORANGE, minw = 5, minh = 0.7, scale = 0.6}),
         },
     }
-end
-
-local game_load_profile_ref = Game.load_profile
-function Game:load_profile(_profile)
-    for _, t in ipairs{
-        G.P_CENTERS,
-        G.P_BLINDS,
-        G.P_TAGS,
-        } do
-        for k, v in pairs(t) do
-            if not v.set or v.set ~= "Ability" then
-                v.discovered = false
-            end
-        end
-    end
-    game_load_profile_ref(self, _profile)
-end
-
-local smods_save_unlocks = SMODS.SAVE_UNLOCKS
-function SMODS.SAVE_UNLOCKS()
-    for _, t in ipairs{
-        G.P_CENTERS,
-        G.P_BLINDS,
-        G.P_TAGS,
-        SMODS.Centers
-        } do
-        for k, v in pairs(t) do
-            if v.discovered == true and v.set ~= "Ability" then
-                v.discovered = false
-            end
-        end
-    end
-    smods_save_unlocks()
-end
-
-G.FUNCS.clear_alerts = function(e)
-    for _, t in ipairs{
-        G.P_CENTERS,
-        G.P_BLINDS,
-        G.P_TAGS,
-        SMODS.Centers
-        } do
-        for _, v in pairs(t) do
-            if v.discovered == true and v.alerted == false then
-                v.alerted = true
-            end
-        end
-    end
-    set_profile_progress()
-    set_discover_tallies()
-    smods_save_unlocks()
-end
-
-G.FUNCS.discover_all = function(e)
-    for _, t in ipairs{
-        G.P_CENTERS,
-        G.P_BLINDS,
-        G.P_TAGS,
-        SMODS.Centers
-        } do
-        for _, v in pairs(t) do
-            if v.unlocked == false then
-                v.unlocked = true
-            end
-            if v.discovered == false then
-                v.discovered = true
-                v.alerted = true
-            end
-        end
-    end
-    if G.P_SKILLS then
-        for _,v in pairs(G.P_SKILLS) do
-            if v.unlocked == false then
-                v.unlocked = true
-            end
-            if v.discovered == false then
-                v.discovered = true
-                v.alerted = true
-            end
-        end
-    end
-    set_profile_progress()
-    set_discover_tallies()
-    smods_save_unlocks()
-end
-
-G.FUNCS.reset_cards = function(e)
-    print("Reset Discovered | Resetting all files")
-    for _, t in ipairs{
-        G.P_CENTERS,
-        G.P_BLINDS,
-        G.P_TAGS,
-        } do
-        for k, v in pairs(t) do
-            v.discovered = false
-            v._discovered_unlocked_overwritten = true
-            if SMODS.Centers[v.key] and SMODS.Centers.discovered == true then
-                SMODS.Centers.discovered = false
-            end
-        end
-    end
-    local meta = STR_UNPACK(get_compressed(G.SETTINGS.profile .. '/' .. 'meta.jkr') or 'return {}')
-    meta.unlocked = {}
-    meta.discovered = {}
-    meta.alerted = {}
-    compress_and_save( G.SETTINGS.profile .. '/'..'meta.jkr', STR_PACK(meta))
-
-    set_profile_progress()
-    set_discover_tallies()
 end
 
 local SMODS_Joker_inject=SMODS.Joker.inject
@@ -367,6 +258,157 @@ print("checking p_fusion_voucher_pack")
 if pack then
     print("Updating p_fusion_voucher_pack")
     pack.discovered = false
+end
+
+function G.UIDEF.profile_option(_profile)
+    set_discover_tallies()
+    G.focused_profile = _profile
+    local profile_data = get_compressed(G.focused_profile..'/'..'profile.jkr')
+        if profile_data ~= nil then
+        profile_data = STR_UNPACK(profile_data)
+        profile_data.name = profile_data.name or ("P".._profile)
+        end
+    G.PROFILES[_profile].name = profile_data and profile_data.name or ''
+
+    local lwidth, rwidth, scale = 1, 1, 1
+    G.CHECK_PROFILE_DATA = nil
+    local t = {n=G.UIT.ROOT, config={align = 'cm', colour = G.C.CLEAR}, nodes={
+        {n=G.UIT.R, config={align = 'cm',padding = 0.1, minh = 0.8}, nodes={
+            ((_profile == G.SETTINGS.profile) or not profile_data) and {n=G.UIT.R, config={align = "cm"}, nodes={
+            create_text_input({
+            w = 4, max_length = 16, prompt_text = localize('k_enter_name'),
+            ref_table = G.PROFILES[_profile], ref_value = 'name',extended_corpus = true, keyboard_offset = 1,
+            callback = function() 
+                G:save_settings()
+                G.FILE_HANDLER.force = true
+            end
+            }),
+        }} or {n=G.UIT.R, config={align = 'cm',padding = 0.1, minw = 4, r = 0.1, colour = G.C.BLACK, minh = 0.6}, nodes={
+            {n=G.UIT.T, config={text = G.PROFILES[_profile].name, scale = 0.45, colour = G.C.WHITE}},
+        }},
+        }},
+        {n=G.UIT.R, config={align = "cm", padding = 0.1}, nodes={
+        {n=G.UIT.C, config={align = "cm", minw = 6}, nodes={
+            (G.PROFILES[_profile].progress and G.PROFILES[_profile].progress.discovered) and create_progress_box(G.PROFILES[_profile].progress, 0.5) or
+            {n=G.UIT.C, config={align = "cm", minh = 4, minw = 5.2, colour = G.C.BLACK, r = 0.1}, nodes={
+            {n=G.UIT.T, config={text = localize('k_empty_caps'), scale = 0.5, colour = G.C.UI.TRANSPARENT_LIGHT}}
+            }},
+        }},
+        {n=G.UIT.C, config={align = "cm", minh = 4}, nodes={
+            {n=G.UIT.R, config={align = "cm", minh = 1}, nodes={
+            profile_data and {n=G.UIT.R, config={align = "cm"}, nodes={
+                {n=G.UIT.C, config={align = "cm", minw = lwidth}, nodes={{n=G.UIT.T, config={text = localize('k_wins'),colour = G.C.UI.TEXT_LIGHT, scale = scale*0.7}}}},
+                {n=G.UIT.C, config={align = "cm"}, nodes={{n=G.UIT.T, config={text = ': ',colour = G.C.UI.TEXT_LIGHT, scale = scale*0.7}}}},
+                {n=G.UIT.C, config={align = "cl", minw = rwidth}, nodes={{n=G.UIT.T, config={text = tostring(profile_data.career_stats.c_wins),colour = G.C.RED, shadow = true, scale = 1*scale}}}}
+            }} or nil,
+            }},
+            {n=G.UIT.R, config={align = "cm", padding = 0.2}, nodes={
+            {n=G.UIT.R, config={align = "cm", padding = 0}, nodes={
+                {n=G.UIT.R, config={align = "cm", minw = 4, maxw = 4, minh = 0.8, padding = 0.2, r = 0.1, hover = true, colour = G.C.BLUE,func = 'can_load_profile', button = "load_profile", shadow = true, focus_args = {nav = 'wide'}}, nodes={
+                {n=G.UIT.T, config={text = _profile == G.SETTINGS.profile and localize('b_current_profile') or profile_data and localize('b_load_profile') or localize('b_create_profile'), ref_value = 'load_button_text', scale = 0.5, colour = G.C.UI.TEXT_LIGHT}}
+                }}
+            }},
+            {n=G.UIT.R, config={align = "cm", padding = 0, minh = 0.7}, nodes={
+                {n=G.UIT.R, config={align = "cm", minw = 3, maxw = 4, minh = 0.6, padding = 0.2, r = 0.1, hover = true, colour = G.C.RED,func = 'can_delete_profile', button = "delete_profile", shadow = true, focus_args = {nav = 'wide'}}, nodes={
+                {n=G.UIT.T, config={text = _profile == G.SETTINGS.profile and localize('b_reset_profile') or localize('b_delete_profile'), scale = 0.3, colour = G.C.UI.TEXT_LIGHT}}
+                }}
+            }},
+            (_profile == G.SETTINGS.profile and not G.PROFILES[G.SETTINGS.profile].all_unlocked) and {n=G.UIT.R, config={align = "cm", padding = 0, minh = 0.7}, nodes={
+                {n=G.UIT.R, config={align = "cm", minw = 3, maxw = 4, minh = 0.6, padding = 0.2, r = 0.1, hover = true, colour = G.C.ORANGE,func = 'can_unlock_all', button = "unlock_all", shadow = true, focus_args = {nav = 'wide'}}, nodes={
+                {n=G.UIT.T, config={text = localize('b_unlock_all'), scale = 0.3, colour = G.C.UI.TEXT_LIGHT}}
+                }}
+            }} or {n=G.UIT.R, config={align = "cm", minw = 3, maxw = 4, minh = 0.7}, nodes={
+                G.PROFILES[_profile].all_unlocked and ((not G.F_NO_ACHIEVEMENTS) and {n=G.UIT.T, config={text = localize(G.F_TROPHIES and 'k_trophies_disabled' or 'k_achievements_disabled'), scale = 0.3, colour = G.C.UI.TEXT_LIGHT}} or 
+                nil) or nil
+            }},
+            {n=G.UIT.R, config={align = "cm", padding = 0, minh = 0.7}, nodes={
+                {n=G.UIT.R, config={align = "cm", minw = 3, maxw = 4, minh = 0.6, padding = 0.2, r = 2.1, hover = true, colour = G.C.GREY,button = "undiscover_all", shadow = true, focus_args = {nav = 'wide'}}, nodes={
+                {n=G.UIT.T, config={text = "Undiscover All", scale = 0.3, colour = G.C.UI.TEXT_LIGHT}}
+                }}
+            }},
+            }},
+        }},
+        }},
+        {n=G.UIT.R, config={align = "cm", padding = 0}, nodes={
+        {n=G.UIT.T, config={id = 'warning_text', text = localize('ph_click_confirm'), scale = 0.4, colour = G.C.CLEAR}}
+        }}
+    }} 
+    return t
+end
+
+G.FUNCS.undiscover_all = function(e)
+    G.PROFILES[G.SETTINGS.profile].all_unlocked = false
+    if G.P_SKILLS then
+        for _, v in pairs(G.P_SKILLS) do
+            if not v.demo and not v.wip then
+                v.alerted = false
+                v.discovered = false
+                v.unlocked = false
+            end
+        end
+    end
+    for _, v in pairs(G.P_CENTERS) do
+    if not v.demo and not v.wip and v.set ~= "Ability" then
+        v.alerted = false
+        v.discovered = false
+        v.unlocked = false
+    end
+    end
+    for _, v in pairs(G.P_BLINDS) do
+    if not v.demo and not v.wip then
+        v.alerted = false
+        v.discovered = false
+        v.unlocked = false
+    end
+    end
+    for _, v in pairs(G.P_TAGS) do
+    if not v.demo and not v.wip then
+        v.alerted = false
+        v.discovered = false
+        v.unlocked = false
+    end
+    end
+    set_profile_progress()
+    set_discover_tallies()
+    G:save_progress()
+    G.FILE_HANDLER.force = true
+
+    local tab_but = G.OVERLAY_MENU:get_UIE_by_ID('tab_but_'..G.focused_profile)
+    G.FUNCS.change_tab(tab_but)
+end
+
+G.FUNCS.clear_all = function(e)
+    if G.P_SKILLS then
+        for _, v in pairs(G.P_SKILLS) do
+            if not v.demo and not v.wip then
+                v.alerted = true
+            end
+        end
+    end
+    for _, v in pairs(G.P_CENTERS) do
+        if not v.demo and not v.wip then
+            v.alerted = true
+        end
+    end
+    for _, v in pairs(G.P_BLINDS) do
+        if not v.demo and not v.wip then
+            v.alerted = true
+        end
+    end
+    for _, v in pairs(G.P_TAGS) do
+        if not v.demo and not v.wip then
+            v.alerted = true
+        end
+    end
+    for _, v in pairs(G.P_SEALS) do
+        if not v.demo and not v.wip then
+            v.alerted = true
+        end
+    end
+    set_profile_progress()
+    set_discover_tallies()
+    G:save_progress()
+    G.FILE_HANDLER.force = true
 end
 
 
